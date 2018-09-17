23daa1f8bce2e7ab9ac9f7dc5daf463b5f7dd325
Michael Murphy <mpmurphy1432@gmail.com>
2018-09-16 22:02:21 -0500

    ask for help

--- DivvyBikes/ViewController.swift
+++ DivvyBikes/ViewController.swift
@@ -7,16 +7,36 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate, CLLocationManagerDelegate {
 
     var locations = ["Hello"]
+    var locationCoordinates: [MKPlacemark] = []
     
+    
+    let locationManager = CLLocationManager()
+
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        mapView.delegate = self
         getJsonData()
+        setupMapView()
+    }
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -27,6 +47,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return locations.count
+    }
+    
+    
+    @IBAction func segmentedControlClicked(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            tableView.isHidden = false
+            mapView.isHidden = true
+        case 1:
+            tableView.isHidden = true
+            mapView.isHidden = false
+        default:
+            tableView.isHidden = false
+            mapView.isHidden = true
+        }
+    }
+    
+    func setupMapView() {
+        self.mapView.isZoomEnabled = true
+        self.mapView.showsUserLocation = true
+        
     }
     
     
@@ -42,6 +83,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 for station in listOfStations {
                     let arrayElement = station as! NSDictionary
                     let stationName = arrayElement.object(forKey: "stationName") as! NSString
+                    let stationLatitude = arrayElement.object(forKey: "latitude") as! Double
+                    let stationLongitude = arrayElement.object(forKey: "longitude") as! Double
+                
+                    DispatchQueue.main.async {
+                        let placemark = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: stationLatitude, longitude: stationLongitude))
+                        self.locationCoordinates.append(placemark)
+
+                        for placemark in self.locationCoordinates {
+                            self.mapView.addAnnotation(placemark)
+                            self.mapView.reloadInputViews()
+                        }
+                    }
+                    
                     self.locations.append(stationName as String)
                 }
             }
